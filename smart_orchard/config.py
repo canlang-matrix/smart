625d02c255fe013337811e6eb1c2dcde263c243a
canlang-matrix <2325950819@qq.com>
2026-01-21 00:32:10 +0800

    Delete smart_orchard directory

--- smart_orchard/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    # 替换你的数据库信息: mysql+pymysql://用户名:密码@地址/数据库名
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:www.cx123@localhost/smart_orchard'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'your_secret_key_123'  # 用于 Session 加密
